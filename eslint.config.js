cb15fe810d9b5032e030b0e253e2b9d140fa8de0
sahil kushwaha <sahilkushwaha5510@gmail.com>
2025-08-17 12:46:18 +0530

    refactor: rename project and update dependencies
    - Changed project name from let_connect_frontend to lets_connect_frontend in package.json.
    - Added Tailwind CSS and DaisyUI as dependencies.
    - Updated ESLint and Prettier configurations.
    - Cleaned up App component and added a sample styled paragraph.
    - Removed custom CSS styles and integrated Tailwind CSS.
    - Updated main entry point to use consistent import style.
    - Configured Vite to use Tailwind CSS plugin.

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,32 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx}"],
     extends: [
       js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
+      // it set for both eslint-plugin-prettier and eslint-config-prettier in one go
+      eslintPluginPrettierRecommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
+        ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        sourceType: "module",
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
     },
   },
-])
+]);
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
+// import { useState } from "react";
 
 function App() {
-
   return (
     <>
       <div>
-
+        <p className="text-red-500">hello</p>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
