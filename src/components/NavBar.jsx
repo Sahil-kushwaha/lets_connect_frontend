bbaf31a5781b52a7cd7f965a1405ab32a1a59f6d
sahil kushwaha <sahilkushwaha5510@gmail.com>
2025-08-20 22:44:39 +0530

    feat:add logut feature

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,43 @@
+import { Link } from "react-router-dom";
 import profile_icon from "../assets/profile_icon.png";
-
+import { useSelector } from "react-redux";
+import { removeUser } from "../store/userSlice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "../utils/constant";
 const NavBar = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+  const captalizeFirst = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+  // logout handler
+  const handleLogout = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/api/v1/auth/logout",
+        {},
+        { withCredentials: true }
+      );
+      dispatch(removeUser());
+      navigate("/login", { replacee: true });
+      console.log(res.data.message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <nav>
       <div>
         <div className="navbar bg-base-200 shadow-sm">
           <div className="flex-1">
-            <a className="btn btn-ghost text-xl">Let's Connect</a>
+            <Link to={"/"} className="btn btn-ghost text-xl">
+              Let's Connect
+            </Link>
           </div>
+          {user && <span>{"Welcome, " + captalizeFirst(user.firstName)}</span>}
           <div className="flex gap-2">
             <div className="dropdown dropdown-end">
               <div
@@ -16,25 +46,49 @@ const NavBar = () => {
                 className="btn btn-ghost btn-circle avatar mx-4"
               >
                 <div className="w-10 rounded-full">
-                  <img alt="profile logo" src={profile_icon} />
+                  <img
+                    alt="profile logo"
+                    src={user ? user.avatarUrl : profile_icon}
+                  />
                 </div>
               </div>
               <ul
                 tabIndex={0}
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
               >
-                <li>
-                  <a className="justify-between">
-                    Profile
-                    <span className="badge">New</span>
-                  </a>
-                </li>
-                <li>
-                  <a>Settings</a>
-                </li>
-                <li>
-                  <a>Logout</a>
-                </li>
+                {user && (
+                  <div>
+                    <li>
+                      <Link to="/profile" className="justify-between">
+                        Profile
+                        <span className="badge">New</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <a>Settings</a>
+                    </li>
+                    <li>
+                      <Link onClick={handleLogout}>Logout</Link>
+                    </li>
+                  </div>
+                )}
+                {!user && (
+                  <div>
+                    <li>
+                      <Link to="/" className="justify-between">
+                        Home
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/login">Login</Link>
+                    </li>
+                    <li>
+                      <Link to="/signup" onClick={handleLogout}>
+                        Signup
+                      </Link>
+                    </li>
+                  </div>
+                )}
               </ul>
             </div>
           </div>
