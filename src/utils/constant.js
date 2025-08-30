3274cfa20d997ae5ac9163c7b6fc40d2d0baf127
sahil kushwaha <sahilkushwaha5510@gmail.com>
2025-08-30 21:45:29 +0530

    fix: restore commented BASE_URL configurations for development and production environments

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,8 @@
-// export const BASE_URL = "http://localhost:3000"; // for lacal
-export const BASE_URL = ""; // i have configure nginx which is act as reverse proxy for my backend /api/ i have follow this covnetion for writing api /api/v1/..;
+// dev
+// export const BASE_URL = "http://localhost:3000";
+
+//production
+// export const BASE_URL = ""; // i have configure nginx which is act as reverse proxy for my backend /api/ i have follow  covnetion for writing api /api/v1/.. that's why i don't include /api as BASE url;
+
+export const BASE_URL =
+  location.hostname === "localhost" ? "http://localhost:3000" : "";
