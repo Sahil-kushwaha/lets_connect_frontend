e268e6546060df57fd8a5d8cf52a1740d23bb29f
sahil kushwaha <sahilkushwaha5510@gmail.com>
2025-08-30 18:55:54 +0530

    fix: update BASE_URL to reflect nginx reverse proxy configuration

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "http://localhost:3000"; // for lacal
+export const BASE_URL = ""; // i have configure nginx which is act as reverse proxy for my backend /api/ i have follow this covnetion for writing api /api/v1/..;
